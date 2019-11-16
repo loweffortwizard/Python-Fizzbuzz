8609eec1c797fac48fd5e5780e41f62173d70d66
Thomas Rowe <51003339+loweffortwizard@users.noreply.github.com>
2019-11-16 22:32:06 +0000

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,22 @@
+#Prog will solve the fizzbuzz problem.
+
+import time
+import sys
+import time
+
+def wait:
+	time.sleep(1)
+
+def main():
+	for int value = 0; value <= 100; value++
+		if(value % 3) && (value & 5):
+			print("Fizzbuzz.")
+		elif(value % 3):
+			print("Fizz.")
+		elif(value & 5):
+			print("Buzz.")
+		else:
+			print("")
+	print("End of prog.")
+	
+main()
